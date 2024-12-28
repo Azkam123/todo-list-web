1a12b963519f5bbf2736a891a96fb27018d080b1
moh hasan <mohhasan9998@gmail.com>
2024-12-28 12:43:18 +0700

    buat routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Grup rute untuk TodoListController
+Route::controller(TodoListController::class)->group(function () {
+    Route::get('/', 'index')->name('todo.index'); // Rute untuk metode index pada URL /
+    Route::resource('todo', TodoListController::class)->except(['index'])->names(['store' => 'todo.store']); // Resource controller tanpa metode index
 });
